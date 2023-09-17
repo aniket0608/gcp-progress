defe9caacb3bd354300dcb13216d59bd4bc87573
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-09-17 13:54:18 +0000

    Added tiers to the count

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -22,19 +22,38 @@ function Count() {
 		<div className="w-full flex justify-center items-center pt-4 py-2 flex-col">
 			<div className="w-[80%] grid grid-cols-3 gap-2 text-normal font-semibold text-gray-700 ">
 				<p className="flex flex-row justify-between p-2 px-4 outline-1 outline outline-blue-700 m-2 rounded-md shadow-md">
-					Total Students : <b className="text-blue-500">{DataCompletes.length}</b>
+					Total Students :{" "}
+					<b className="text-blue-500">{DataCompletes.length}</b>
 				</p>
 				<p className="flex flex-row justify-between p-2 px-4 outline-1 outline outline-blue-700 m-2 rounded-md shadow-md">
-					Total Redemptions : <b className="text-blue-500">{redeemedStudents.length}</b>
+					Total Redemptions :{" "}
+					<b className="text-blue-500">{redeemedStudents.length}</b>
 				</p>
 				<p className="flex flex-row justify-between p-2 px-4 outline-1 outline outline-blue-700 m-2 rounded-md shadow-md">
-					Total GenAI Completions : <b className="text-blue-500">{genAICompletions.length}</b>
+					Total GenAI Completions :{" "}
+					<b className="text-blue-500">{genAICompletions.length}</b>
 				</p>
 				<p className="flex flex-row justify-between p-2 px-4 outline-1 outline outline-blue-700 m-2 rounded-md shadow-md">
-					Total GCCF Completions : <b className="text-blue-500">{GCCFCompletions.length}</b>
+					Total GCCF Completions :{" "}
+					<b className="text-blue-500">{GCCFCompletions.length}</b>
 				</p>
 				<p className="flex flex-row justify-between p-2 px-4 outline-1 outline outline-blue-700 m-2 rounded-md shadow-md">
-					Totality Completions : <b className="text-blue-500">{totalCompletions.length}</b>
+					Totality Completions :{" "}
+					<b className="text-blue-500">{totalCompletions.length}</b>
+				</p>
+				<p className="flex flex-row justify-between p-2 px-4 outline-1 outline outline-blue-700 m-2 rounded-md shadow-md">
+					Tier :{" "}
+					<b className="text-blue-500">
+						{totalCompletions.length < 40
+							? "NA"
+							: totalCompletions.length >= 40 &&
+							  totalCompletions.length < 60
+							? "Three"
+							: totalCompletions.length >= 60 &&
+							  totalCompletions.length < 80
+							? "Two"
+							: "One"}
+					</b>
 				</p>
 			</div>
 		</div>
